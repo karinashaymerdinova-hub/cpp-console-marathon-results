ff0451d2c4b309899dd1cb256f2884003dd8af34
Karina <karinashaymerdinova@gmail.com>
2026-06-17 10:44:34 +0300

    Создана заготовка проекта

---
+++ cpp-console-marathon-results/main-project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    cout << "=== Программа обработки результатов марафона ===" << endl;
+    cout << "Вариант 1: Результаты марафона" << endl;
+    cout << "Автор: Шаймердинова Карина" << endl;
+    cout << "Группа: 25ИСиТ" << endl;
+    return 0;
+}
