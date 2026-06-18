9eec8e2a6f8cb669e26661627cfd0e2fc4280a38
Karina <karinashaymerdinova@gmail.com>
2026-06-18 14:53:31 +0300

    Реализована функция обработки данных

--- cpp-console-marathon-results/main-project/processing.cpp
+++ cpp-console-marathon-results/main-project/processing.cpp
@@ -1,5 +1,16 @@
 #include "processing.h"
 
 int process(const MarathonRecord* records, int count) {
-    return 0;
+    if (count == 0) return -1;
+
+    int minTime = getRaceTimeSec(&records[0]);
+
+    for (int i = 1; i < count; i++) {
+        int t = getRaceTimeSec(&records[i]);
+        if (t < minTime) {
+            minTime = t;
+        }
+    }
+
+    return minTime;
 }
